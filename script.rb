9cdf6e1b97905c39994e7a92ec9afcc14abda7f3
sinework <sine.s.amare@gmail.com>
2020-09-22 11:57:34 +0300

    Add: Added the my_map, my_inject and multiply_els methods. Fixed linter errors

--- script.rb
+++ script.rb
@@ -1,4 +1,3 @@
-# rubocop: disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 module Enumerable
   # -----------my_each methods--------------
 
@@ -59,56 +58,95 @@ module Enumerable
     end
     true
   end
-#-------------------my_any?-----------------------
+  #-------------------my_any?-----------------------
 
-def my_any?(args=nil)
-  if args.nil? == false && args.is_a?(Class) == false
-    if args.is_a?(Regexp)
-      my_each { |i| return true if !i[args].nil? || i[args] == 1 } # if Regexp
+  def my_any?(args = nil)
+    if args.nil? == false && args.is_a?(Class) == false
+      if args.is_a?(Regexp)
+        my_each { |i| return true if !i[args].nil? || i[args] == 1 } # if Regexp
+      else
+        my_each { |i| return true if i == args }
+      end
+    elsif args.is_a?(Class)
+      my_each { |i| return true if i.is_a?(args) }
+
+    # if not Regexp
+
+    elsif args.nil? && block_given?
+      my_each { |i| return true if yield i }
     else
-      my_each { |i| return true if i == args }
+      my_each { |i| return false unless i.nil? || i == false }
     end
-  elsif args.is_a?(Class)
-    my_each { |i| return true if i.is_a?(args) }
-
-  # if not Regexp
-
-  elsif args.nil? && block_given?
-    my_each { |i| return true if yield i }
-  else
-    my_each { |i| return false unless i.nil? || i == false }
+    false
   end
-  false
-end
-#-------------------my_none?-----------------------
-def my_none?(args = nil)
-  if args.nil? == false && args.is_a?(Class) == false
-    if args.is_a?(Regexp)
-      my_each { |i| return false unless i[args].nil? || i[args] != 1 } # if Regexp
+
+  #-------------------my_none?-----------------------
+  def my_none?(args = nil)
+    if args.nil? == false && args.is_a?(Class) == false
+      if args.is_a?(Regexp)
+        my_each { |i| return false unless i[args].nil? || i[args] != 1 } # if Regexp
+      else
+        my_each { |i| return false unless i != args }
+      end
+    elsif args.is_a?(Class)
+      my_each { |i| return false if i.is_a?(args) }
+
+    elsif args.nil? && block_given?
+      my_each { |i| return false if yield i }
     else
-      my_each { |i| return false unless i != args }
+      my_each { |i| return false unless i.nil? || i != false }
     end
-  elsif args.is_a?(Class)
-    my_each { |i| return false unless !i.is_a?(args) }
-
-  elsif args.nil? && block_given?
-    my_each { |i| return false unless !yield i }
-  else
-    my_each { |i| return false unless i.nil? || i != false }
+    true
   end
-  true
-end
-#-------------------my_count-----------------------
-def my_count(args = nil)
-  count=0
-  if !args.nil?
-    my_each{|x| count += 1 if x==args}
-  elsif args.nil? && block_given?
-    my_each { |x| count +=1 if yield (x) }
-   else my_each{count += 1 }
-   end 
-   count   
-end
 
- # rubocop: enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+  #-------------------my_count-----------------------
+  def my_count(args = nil)
+    count = 0
+    if !args.nil?
+      my_each { |x| count += 1 if x == args }
+    elsif args.nil? && block_given?
+      my_each { |x| count += 1 if yield x }
+    else my_each { count += 1 }
+    end
+    count
+  end
+
+  #-------------------my_map-----------------------
+  def my_map(args = nil)
+    return to_enum(:my_map) if args.nil? && !block_given?
+
+    mapped = []
+    if args.nil?
+      my_each_with_index { |i, j| mapped[j] = yield i }
+    else
+      my_each_with_index { |i, j| mapped[j] = args.call(i) }
+    end
+    mapped
+  end
+  #-------------------my_inject-----------------------
+
+  def my_inject(arg1 = nil, arg2 = nil)
+    if block_given?
+      acc = arg1
+      my_each { |item| acc = acc.nil? ? item : yield(acc, item) }
+      acc
+    elsif arg1.is_a?(Symbol) || arg1.is_a?(String)
+      acc = nil
+      my_each { |item| acc = acc.nil? ? item : acc.send(arg1, item) }
+      acc
+    elsif arg2.is_a?(Symbol) || arg2.is_a?(String)
+      acc = arg1
+      my_each { |item| acc = acc.nil? ? item : acc.send(arg2, item) }
+      acc
+    else
+      raise LocalJumpError, 'No block Given or Empty Argument' unless !arg1.nil? && !arg2.nil? && !block_given?
+    end
+  end
+
+  #-------------------multiply_els-----------------------
+  def multiply_els(arr)
+    raise TypeError, 'No Array Given' if arg.nil? || !arg.is_a?(Array)
+
+    arr.my_inject { |result, element| result * element }
+  end
 end
